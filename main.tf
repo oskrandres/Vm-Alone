8e07dafddea656c8e863fe88cf584272f089010c
Oscar Medina Aguilar <129307432+oskrandres@users.noreply.github.com>
2024-11-04 10:16:11 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "azurerm" {
   features {}
-  subscription_id = "18bf3718-f695-4c96-8d45-72dc2e617d2e"
-  tenant_id       = "16b3c013-d300-468d-ac64-7eda0820b6d3"
+  subscription_id = ""
+  tenant_id       = ""
 }
 
 resource "azurerm_resource_group" "rg" {
