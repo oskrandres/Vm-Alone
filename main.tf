ccf011917c55d31cf685a7b5437ca70c30a800f5
Oscar Medina Aguilar <129307432+oskrandres@users.noreply.github.com>
2024-11-04 10:15:43 -0500

    Add files via upload

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "myResourceGroup"
+  name     = "VMalone"
   location = "Canada Central"
 }
 
@@ -27,7 +27,8 @@ resource "azurerm_public_ip" "public_ip" {
   name                = "myPublicIP"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static"
+  sku                 = "Standard"
 }
 
 resource "azurerm_network_interface" "nic" {
